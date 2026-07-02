c32b6c18655f4f1a7c98d590ac7d87b390b458cc
KevinDZ <2584307453@github.com>
2026-07-02 13:29:19 +0800

    Normalize line endings to LF

---
+++ vehicle/internal/logic/vehicleservice/controlvehiclelogic.go
@@ -0,0 +1,31 @@
+package vehicleservicelogic
+
+import (
+	"context"
+
+	"vehicle/internal/svc"
+	"vehicle/vehicle/vehicle"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ControlVehicleLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewControlVehicleLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ControlVehicleLogic {
+	return &ControlVehicleLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 接收云端下发的控制指令
+func (l *ControlVehicleLogic) ControlVehicle(in *vehicle.ControlReq) (*vehicle.ControlResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &vehicle.ControlResp{}, nil
+}
